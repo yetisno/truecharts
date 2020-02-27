7f24bd993821e747f9b308dee85992209698cb47
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-02-27 20:30:33 +0100

    bug fixes

--- update/organizr.sh
+++ update/organizr.sh
@@ -2,11 +2,9 @@ iocage exec organizr service nginx stop
 iocage exec organizr service php-fpm stop
 iocage update organizr
 iocage exec organizr "pkg update && pkg upgrade -y"
-mv /mnt/tank/iocage/jails/organizr/root/usr/local/etc/nginx/nginx.conf /mnt/tank/iocage/jails/organizr/root/usr/local/etc/nginx/nginx.conf.bak
 cp ../includes/organizr-conf/nginx.conf /mnt/tank/iocage/jails/organizr/root/usr/local/etc/nginx/nginx.conf
-cp ../includes/organizr-conf/custom /mnt/tank/iocage/jails/organizr/root/usr/local/etc/nginx/custom
-iocage exec tautulli cd /usr/local/www/Organizr
-iocage exec organizr git pull
+cp ../includes/organizr-conf/custom/*.* /mnt/tank/iocage/jails/organizr/root/usr/local/etc/nginx/custom/
+iocage exec organizr "cd /usr/local/www/Organizr && git pull"
 iocage exec organizr chown -R www:www /usr/local/www /config /usr/local/etc/nginx/nginx.conf /usr/local/etc/nginx/custom
 iocage exec organizr service nginx start
 iocage exec organizr service php-fpm start
--- update/tautulli.sh
+++ update/tautulli.sh
@@ -1,8 +1,7 @@
 iocage exec tautulli service tautulli stop
 iocage update tautulli
 iocage exec tautulli "pkg update && pkg upgrade -y"
-iocage exec tautulli cd /usr/local/share/Tautulli
-iocage exec tautulli git pull
+iocage exec tautulli "cd /usr/local/share/Tautulli && git pull"
 iocage exec tautulli chown -R tautulli:tautulli /usr/local/share/Tautulli /config
 iocage exec tautulli cp /usr/local/share/Tautulli/init-scripts/init.freenas /usr/local/etc/rc.d/tautulli
 iocage exec tautulli chmod u+x /usr/local/etc/rc.d/tautulli
