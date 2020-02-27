3b3f22f557f05ed68e9f1f2afaf7099dce914bab
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-02-27 20:47:01 +0100

    bugz

--- install/lidarr.sh
+++ install/lidarr.sh
@@ -9,8 +9,8 @@ iocage fstab -a lidarr /mnt/tank/downloads/complete /mnt/fetched nullfs rw 0 0
 iocage fstab -a lidarr /mnt/tank/library/Music /mnt/music nullfs rw 0 0
 iocage exec lidarr ln -s /usr/local/bin/mono /usr/bin/mono
 iocage exec lidarr "fetch https://github.com/lidarr/Lidarr/releases/download/v0.2.0.371/Lidarr.develop.0.2.0.371.linux.tar.gz -o /usr/local/share"
-iocage exec lidarr "tar -xzvf /usr/local/share/v0.2.0.371/Lidarr.develop.0.2.0.371.linux.tar.gz -C /usr/local/share"
-iocage exec lidarr "rm /usr/local/share/v0.2.0.371/Lidarr.develop.0.2.0.371.linux.tar.gz"
+iocage exec lidarr "tar -xzvf /usr/local/share/Lidarr.develop.0.2.0.371.linux.tar.gz -C /usr/local/share"
+iocage exec lidarr "rm /usr/local/share/Lidarr.develop.0.2.0.371.linux.tar.gz"
 iocage exec lidarr "pw user add lidarr -c lidarr -u 353 -d /nonexistent -s /usr/bin/nologin"
 iocage exec lidarr chown -R lidarr:lidarr /usr/local/share/Lidarr /config
 iocage exec lidarr mkdir /usr/local/etc/rc.d
