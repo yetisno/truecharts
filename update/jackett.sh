c3669c5d30953bfb5f02892524ec7dc7029080fd
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-02-27 20:42:01 +0100

    remove update from apps with in-app updater

--- update/jackett.sh
+++ update/jackett.sh
@@ -1,10 +1,7 @@
 iocage exec jackett service jackett stop
 iocage update jackett
 iocage exec jackett "pkg update && pkg upgrade -y"
-iocage exec jackett "fetch https://github.com/Jackett/Jackett/releases/download/v0.11.502/Jackett.Binaries.Mono.tar.gz -o /usr/local/share"
-iocage exec jackett "tar -xzvf /usr/local/share/Jackett.Binaries.Mono.tar.gz -C /usr/local/share"
-iocage exec jackett rm /usr/local/share/Jackett.Binaries.Mono.tar.gz
 iocage exec jackett chown -R jackett:jackett /usr/local/share/Jackett /config
 cp ../includes/jackett-conf/jackett.rc /mnt/tank/iocage/jails/jackett/root/usr/local/etc/rc.d/jackett
 iocage exec jackett chmod u+x /usr/local/etc/rc.d/jackett
-iocage exec jackett service jackett start
+iocage exec jackett service jackett restart
--- update/plex.sh
+++ update/plex.sh
@@ -2,12 +2,7 @@ iocage exec plex service plexmediaserver_plexpass stop
 iocage update plex
 iocage exec plex "pkg update && pkg upgrade -y"
 iocage exec plex chown -R plex:plex /usr/local/share/plexmediaserver-plexpass/
-iocage exec plex service plexmediaserver_plexpass start
+iocage exec plex service plexmediaserver_plexpass restart
 sleep 10s
-echo "Waiting 20s to stop plex"
-iocage stop plex
-sleep 10s
-echo "Waiting 20s to start plex"
-iocage start plex
 iocage exec plex service plexmediaserver_plexpass start
-echo "Finished installing plex"
+echo "Finished updating plex"
