673df1c2b7152a5fc22478777b0f61c8af7c438e
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-02-27 18:12:07 +0100

    Added -arr services and updates organizr

---
+++ radarr.sh
@@ -0,0 +1,20 @@
+echo '{"pkgs":["mono","mediainfo","sqlite3","ca_root_nss","curl","nano"]}' > /tmp/pkg.json
+iocage create -n "radarr" -p /tmp/pkg.json -r 11.3-RELEASE interfaces="vnet0:bridge30" ip4_addr="vnet0|192.168.30.31/24" defaultrouter="192.168.30.1" vnet="on" allow_raw_sockets="1" boot="on"
+rm /tmp/pkg.json
+iocage exec radarr mkdir -p /config
+iocage exec radarr mkdir -p /mnt/music
+iocage exec radarr mkdir -p /mnt/fetched
+iocage fstab -a radarr /mnt/tank/apps/radarr /config nullfs rw 0 0
+iocage fstab -a radarr /mnt/tank/downloads/complete /mnt/fetched nullfs rw 0 0
+iocage fstab -a radarr /mnt/tank/library/Movies /mnt/movies nullfs rw 0 0
+iocage exec radarr ln -s /usr/local/bin/mono /usr/bin/mono
+iocage exec radarr "fetch https://github.com/Radarr/Radarr/releases/download/v0.2.0.995/Radarr.develop.0.2.0.995.linux.tar.gz -o /usr/local/share"
+iocage exec radarr "tar -xzvf /usr/local/share/Radarr.*.linux.tar.gz -C /usr/local/share"
+iocage exec radarr rm /usr/local/share/Radarr.*.linux.tar.gz
+iocage exec radarr "pw user add radarr -c radarr -u 352 -d /nonexistent -s /usr/bin/nologin"
+iocage exec radarr chown -R radarr:radarr /usr/local/share/Radarr /config
+iocage exec radarr mkdir /usr/local/etc/rc.d
+cp ./radarr-conf/radarr.rc /mnt/tank/iocage/jails/radarr/root/usr/local/etc/rc.d/radarr
+iocage exec radarr chmod u+x /usr/local/etc/rc.d/radarr
+iocage exec radarr sysrc "radarr_enable=YES"
+iocage exec radarr service radarr start
---
+++ sonarr.sh
@@ -0,0 +1,20 @@
+echo '{"pkgs":["mono","mediainfo","sqlite3","ca_root_nss","curl","nano"]}' > /tmp/pkg.json
+iocage create -n "sonarr" -p /tmp/pkg.json -r 11.3-RELEASE interfaces="vnet0:bridge30" ip4_addr="vnet0|192.168.30.30/24" defaultrouter="192.168.30.1" vnet="on" allow_raw_sockets="1" boot="on"
+rm /tmp/pkg.json
+iocage exec sonarr mkdir -p /config
+iocage exec sonarr mkdir -p /mnt/music
+iocage exec sonarr mkdir -p /mnt/fetched
+iocage fstab -a sonarr /mnt/tank/apps/sonarr /config nullfs rw 0 0
+iocage fstab -a sonarr /mnt/tank/downloads/complete /mnt/fetched nullfs rw 0 0
+iocage fstab -a sonarr /mnt/tank/library/Series /mnt/series nullfs rw 0 0
+iocage exec sonarr ln -s /usr/local/bin/mono /usr/bin/mono
+iocage exec sonarr "fetch http://download.sonarr.tv/v2/master/mono/NzbDrone.master.tar.gz -o /usr/local/share"
+iocage exec sonarr "tar -xzvf /usr/local/share/NzbDrone.master.tar.gz -C /usr/local/share"
+iocage exec sonarr rm /usr/local/share/NzbDrone.master.tar.gz
+iocage exec sonarr "pw user add sonarr -c sonarr -u 351 -d /nonexistent -s /usr/bin/nologin"
+iocage exec sonarr chown -R sonarr:sonarr /usr/local/share/NzbDrone /config
+iocage exec sonarr mkdir /usr/local/etc/rc.d
+cp ./sonarr-conf/sonarr.rc /mnt/tank/iocage/jails/sonarr/root/usr/local/etc/rc.d/sonarr
+iocage exec sonarr chmod u+x /usr/local/etc/rc.d/sonarr
+iocage exec sonarr sysrc "sonarr_enable=YES"
+iocage exec sonarr service sonarr start
