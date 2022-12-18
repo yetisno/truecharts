6ad4aa373cb8378935ef9e9818ed77c6c2cac82e
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-12-18 11:40:16 +0100

    fix hotpatch

--- includes/patch.sh
+++ includes/patch.sh
@@ -2,7 +2,7 @@
 
 patchv22120(){
 echo "Applying 22.12 HotPatch 1"
-( wget -q -P /tmp https://github.com/truecharts/truetool/raw/master/hotpatch/2212/HP1.patch && echo "download completed" || echo "download failed" ) && ( patch -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < /tmp/HP1.patch && echo "patch completed" || echo "patch failed" ) && rm -rf /tmp/HP1.patch
+( wget -q -P /tmp https://github.com/truecharts/truetool/raw/main/hotpatch/2212/HP1.patch && echo "download completed" || echo "download failed" ) && ( patch -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < /tmp/HP1.patch && echo "patch completed" || echo "patch failed" ) && rm -rf /tmp/HP1.patch
 }
 export -f patchv22120
 
