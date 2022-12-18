864c3154a6cf4e6adba92999bfae9d5164dfbac1
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-12-18 12:09:29 +0100

    ensure middleware restart after successfull patch

--- includes/patch.sh
+++ includes/patch.sh
@@ -2,7 +2,7 @@
 
 patchv22120(){
 echo "Applying 22.12 HotPatch 1"
-( wget -q -P /tmp https://github.com/truecharts/truetool/raw/main/hotpatch/2212/HP1.patch && echo "download completed" || echo "download failed" ) && ( patch -N -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < /tmp/HP1.patch && echo "patch completed" || echo "patch failed or skipped, not critical" ) && rm -rf /tmp/HP1.patch
+( wget -q -P /tmp https://github.com/truecharts/truetool/raw/main/hotpatch/2212/HP1.patch && echo "download completed" || echo "download failed" ) && ( patch -N -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < /tmp/HP1.patch && service middlewared restart && echo "waiting 20 seconds for middleware restart..." && sleep 20 && echo "patch completed" || echo "patch failed or skipped, not critical" ) && rm -rf /tmp/HP1.patch
 }
 export -f patchv22120
 
