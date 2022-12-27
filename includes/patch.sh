3e4ec91b3fa03b5de0c127d123fcfb4f2410ccaa
Kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2022-12-27 13:45:09 +0100

    attempt 3

--- includes/patch.sh
+++ includes/patch.sh
@@ -3,7 +3,7 @@
 patchv22120(){
 echo "Applying 22.12 HotPatch 1"
 
-( wget -q -P /tmp https://github.com/truecharts/truetool/raw/main/hotpatch/2212/HP1.patch && echo "download completed" || echo "download failed" ) && ( patch -N -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ &>/dev/null < /tmp/HP1.patch && echo "patch completed" && restartmiddleware=yes || echo "Patch Already Applied" ) && rm -rf /tmp/HP1.patch
+( wget -q -P /tmp https://github.com/truecharts/truetool/raw/main/hotpatch/2212/HP1.patch && echo "download completed" || echo "download failed" ) && ( patch -N -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ &>/dev/null < /tmp/HP1.patch && echo "patch completed" || echo "Patch Already Applied" ) && rm -rf /tmp/HP1.patch
 
 echo "Applying 22.12 HotPatch 2"
 ( wget -q -P /tmp https://github.com/truecharts/truetool/raw/main/hotpatch/2212/HP2.patch && echo "download completed" || echo "download failed" ) && ( patch -N -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ &>/dev/null < /tmp/HP2.patch && echo "patch completed" && restartmiddleware=yes || echo "Patch Already Applied" ) && rm -rf /tmp/HP2.patch
@@ -22,7 +22,7 @@ else
   echo "No hotpatch available for your version, congratulations!"
 fi
 
-if (( "$restartmiddleware" == "yes" )); then
+if [[ "$restartmiddleware" == "yes" ]]; then
   middlewareRestart
 fi
 restartmiddleware=no
